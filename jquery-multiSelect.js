0b4330bf2f46e89f29810a1d154e7ac4c4497aec
Vianney Dubus <vianney.dubus@gmail.com>
2014-11-22 12:24:54 +0100

    Manage sort order
    Manage the sorting of the elements in function on their order in the
    original select.
    + Manage the internal structure to separate le cloned select from
    selected span.

--- jquery-multiSelect.js
+++ jquery-multiSelect.js
@@ -27,6 +27,11 @@
 		_container : null,
 
 		/**
+		 * The container which will contain the spans.
+		 */
+		_spanContainer : null,
+
+		/**
 		 * The current name of the select.
 		 */
 		_elementName : null,
@@ -46,7 +51,9 @@
 			}
 			this._elementName = this.element.prop("name");
 			this._select = this.element.clone();
-			this._container = $("<span />").addClass("vdubus-multiSelect").append(this._select);
+			var selectContainer = $("<span />").addClass("vdubus-multiSelect-selectContainer").append(this._select);
+			this._spanContainer = $("<span />").addClass("vdubus-multiSelect-spanContainer");
+			this._container = $("<span />").addClass("vdubus-multiSelect").append(selectContainer).append(this._spanContainer);
 			this.element.addClass("vdubus-multiSelect-hidden").after(this._container);
 
 			this._on(this._container, {
@@ -54,7 +61,7 @@
 			});
 			if (this.options.enableChangeMode) {
 				// We must manage the empty option.
-				var emptyOption = $("<option />").addClass("vdubus-multiSelect-emptyOption").attr("value", "");
+				var emptyOption = $("<option />").addClass("vdubus-multiSelect-emptyOption").attr("value", "").data("index", -1);
 				if (this.options.emptyOptionLabel) {
 					// If a label is available, then we use it.
 					emptyOption.text(this.options.emptyOptionLabel);
@@ -71,12 +78,20 @@
 				});
 			}
 
+			// Each option should have an index in function of their position in the select.
+			var index = 0;
+			var options = this._select.find("option");
+			for (; index < options.length; index++) {
+				var tmpOpt = $(options[index]);
+				tmpOpt.data("index", index);
+			}
+
 			// Manage the currents selected values.
 			this._manageSelectedValue();
 
 			// Remove unneeded attributes once we have synchronized our component.
 			this._select.removeAttr("multiple").removeAttr("name").removeAttr("id");
-			
+
 			if (this.options.enableChangeMode) {
 				// For Chrome compatibility, once we have manage the current selected value, we must select the first one.
 				this._select.find("option:first").prop("selected", true);
@@ -99,9 +114,12 @@
 		 * @param {jQuery} spanValue : the value to move back.
 		 */
 		_moveBackValue : function(spanValue) {
-			var inputHidden = spanValue.find("input");
-			$("<option />").attr("value", inputHidden.val()).text(spanValue.find("span.vdubus-multiSelect-value-text").text()).appendTo(this._select);
-			this.element.find("option[value='" + inputHidden.val() + "']").prop("selected", false).trigger("change");
+			var value = spanValue.data("value");
+			var optToInsert = $("<option />").attr("value", value).text(spanValue.find("span.vdubus-multiSelect-value-text").text()).data("index", spanValue.data("index"));
+			this._insertElementInOrder(optToInsert, this._select, "option");
+			this.element.find("option[value='" + value + "']").prop("selected", false).trigger("change");
+			
+			// We must remove the element after we getting the data on it because the remove function delete them.
 			spanValue.remove();
 		},
 
@@ -119,18 +137,58 @@
 		 * Manage the current selected values to display them under the select.
 		 */
 		_manageSelectedValue : function() {
-			var selectedOptions = this._select.find("option").filter(":selected").remove();
+			var selectedOptions = this._select.find("option").filter(":selected");
 			var index = 0;
 			for (; index < selectedOptions.length; index++) {
 				var option = $(selectedOptions[index]);
-				var input = $("<input />").attr("type", "hidden").attr("value", option.prop("value"));
+				var indexOpt = option.data("index");
 				var button = $("<span />").addClass("vdubus-multiSelect-value-delete ui-icon ui-icon-trash");
 				var span = $("<span />").addClass("vdubus-multiSelect-value-text").text(option.text());
-				$("<span />").addClass("vdubus-multiSelect-value").append(input).append(span).append(button).appendTo(this._container);
+				var spanToInsert = $("<span />").addClass("vdubus-multiSelect-value").append(span).append(button).data("index", indexOpt).data("value", option.prop("value"));
+				this._insertElementInOrder(spanToInsert, this._spanContainer, "span.vdubus-multiSelect-value");
 				this.element.find("option[value='" + option.prop("value") + "']").prop("selected", true);
 			}
+
+			// We must remove the element after we getting the data on it because the remove function delete them.
+			selectedOptions.remove();
 			if (index) {
 				this.element.trigger("change");
+			}
+		},
+
+		/**
+		 * Insert an element in the order specified by data("index") into another element.
+		 * 
+		 * @param {jQuery} elementToInsert : the element to insert.
+		 * @param {jQuery} elementForInsert : the element for the insert.
+		 * @param {String} selector : the selector to find elements for insertion.
+		 */
+		_insertElementInOrder : function(elementToInsert, elementForInsert, selector) {
+			var elements = elementForInsert.find(selector);
+			var indexToInsert = elementToInsert.data("index") * 1;
+
+			// We must find the position in which we must insert our element.
+			var index = 0;
+			var element = null;
+			var inserted = false;
+			for (; index < elements.length && !inserted; index++) {
+				element = $(elements[index]);
+				var elementIndex = element.data("index") * 1;
+				if (elementIndex > indexToInsert) {
+					element.before(elementToInsert);
+					inserted = true;
+				}
+			}
+
+			// Our element wasn't inserted, so it must be the first or the last.
+			if (!inserted) {
+				if (element) {
+					// Our element was the first.
+					element.after(elementToInsert);
+				} else {
+					// Our element was the last.
+					elementToInsert.appendTo(elementForInsert);
+				}
 			}
 		},
 
